dc13338605c868c7a586ccdfff1578093b62c04a
StombieIT <stombie@yandex.ru>
2022-11-22 16:26:24 +0300

    todos widget switcher fixed

--- common.blocks/todos-widget/__footer-section-switcher/todos-widget__footer-section-switcher.js
+++ common.blocks/todos-widget/__footer-section-switcher/todos-widget__footer-section-switcher.js
@@ -1,10 +1,8 @@
 import { store } from "../../../store";
 import { migrate } from "../todos-widget";
 
-const todosWidgetFooterSectionSwitcherInputs = [...document.querySelectorAll(".todos-widget__footer-section-switcher")]
-    .map(switcher => switcher.querySelector("input"))
-
-todosWidgetFooterSectionSwitcherInputs.forEach(input => input.addEventListener("change", evt => {
-    store.todosType = evt.target.value
-    migrate(store)
-}))
+document.querySelector(".js-todos-type-container")
+    .addEventListener("change", evt => {
+        store.todosType = evt.target.value
+        migrate(store)
+    })
