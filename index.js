ce43441e784a40cf0a5c61560d018b13ef382be4
StombieIT <stombie@yandex.ru>
2022-11-23 11:13:45 +0300

    imports fixed

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 import * as todosWidget from "./common.blocks/todos-widget/todos-widget.js"
-import * as todosWidgetHeaderForm from "./common.blocks/todos-widget/__header-form/todos-widget__header-form"
-import * as todosWidgetBtnActionSelectAll from "./common.blocks/todos-widget-btn/_action/todos-widget-btn_action_select-all"
+import * as todosWidgetHeaderForm from "./common.blocks/todos-widget/__header-form/todos-widget__header-form.js"
+import * as todosWidgetBtnActionSelectAll from "./common.blocks/todos-widget-btn/_action/todos-widget-btn_action_select-all.js"
 import * as todosWidgetTodosTypeContainer from "./common.blocks/todos-widget-todos-type-container.js"
